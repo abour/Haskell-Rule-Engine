fbfb8c2872643ced78901e4974eb3a6252c9a8d4
= <=>
2015-09-11 11:21:27 +0200

    Add few comments on Main.hs file

--- Main.hs
+++ Main.hs
@@ -6,8 +6,8 @@ main = do
     let operator = InsideOp (4::Int)
     let coupleValue = CoupleVal (2::Int) (5::Int)
     let boxing = Template coupleValue operator
-    let condition = Condition "Alaintérieure" boxing
+    let condition = Condition "Just a simple descriptive label" boxing
     let rule = addEvaluable (Rule []) condition
     let ruleEngine = addRule (RuleEngine []) rule
 
-    print (evalRuleEngine ruleEngine)
+    print (evalRuleEngine ruleEngine) -- Display "True". Yes 4 is inside the interval [2, 5] :D
